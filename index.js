c0ae083f96a15638f7aa97dcbe7cc93a1e94e475
Albert-Byrone <albertbyrone1677@gmail.com>
2021-08-22 11:04:00 +0300

    minor updates

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let res = document.getElementById("regForm");
-console.log(res)
+function regUser(e) {
+  e.preventDefault();
+  let email = document.getElementById("email").value;
+  let passwd = document.getElementById("psw").value;
+  let confirm_passwd = document.getElementById("psw-repeat").value;
+
+  if (email == "") {
+    console.log("`EMpty not requird");
+  }
+
+  console.log(email);
+  console.log(passwd);
+  console.log(confirm_passwd);
+}
